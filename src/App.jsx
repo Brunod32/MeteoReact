2dbcb6ff1e68548ae7a031cbf946b3d9bebf6c1b
Bruno Dahlem <bruno.dahlem2@gmail.com>
2024-03-04 22:05:05 +0100

    Initialization project, with date

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <p>Application de météo en react</p>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,53 @@
+import { useEffect, useState } from 'react';
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+    border-bottom: 5px solid white;
+    margin: 0rem 5rem;
+`;
+
+const Title = styled.h1`
+    margin: 1rem 0rem;
+    color: blueviolet;
+    text-align: center;
+`;
+
+const DateWrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    font-size: 1.5rem;
+    margin: 3rem;
+    text-align: center;
+`;
+
+function Header() {
+
+    const [currentTime, setCurrentTime] = useState(new Date());
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentTime(new Date());
+        }, 1000);
+
+        return () => clearInterval(interval);
+    }, [])
+
+    const options = { day: '2-digit', month: 'long', year: 'numeric' };
+    const formattedCurrentDate = new Date().toLocaleDateString('fr-FR', options);
+    const hours = currentTime.getHours();
+    const minutes = currentTime.getMinutes();
+    const secondes = currentTime.getSeconds();
+
+
+    return (
+        <>
+            <Wrapper>
+                <Title>Météo</Title>
+                <DateWrapper>Nos sommes le {formattedCurrentDate} et il est {hours}:{minutes}:{secondes}</DateWrapper>
+            </Wrapper>
+        </>
+    )
+
+}
+
+export default Header;
