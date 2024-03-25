b645f44236ff23395ed2db0b1625741c96dc5034
Bruno Dahlem <bruno.dahlem2@gmail.com>
2024-03-25 21:34:47 +0100

    added reaction icons, performed searchweather and localizedweather

--- src/components/LocalizedMeteo.jsx
+++ src/components/LocalizedMeteo.jsx
@@ -15,6 +15,11 @@ const WeatherWrapper = styled.div`
     justify-content: center;
     align-items: center;
     gap: 50px;
+
+    @media (max-width: 900px) {
+        flex-wrap: wrap;
+        justify-content: space-around;
+    }
 `
 const WeatherTile = styled.div`
     border: 2px solid blueviolet;
@@ -28,32 +33,30 @@ const WeatherTile = styled.div`
     height: 15rem;
     font-size: 1.5rem;
     background-color: #5f5d5d;
+    background-image: ${(props) => props.backgroundurl ? `linear-gradient(rgba(95, 93, 93, 0.5), rgba(95, 93, 93, 0.5)), url(${props.backgroundurl})` : 'none'};
+    background-size: cover;
+    background-position: center;
+    color: black;
+    font-weight: bold;
+    background-color: rgba(95, 93, 93, 0.5);
+
+    p {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .bi {
+        font-size: 2em;
+    }
 `
-// #city .bi {
-//     font-size: 2em;
-// }
-
-// .bi-thermometer-half {
-//     font-size: 2em;
-//     color: red;
-// }
-
-// .bi-wind {
-//     font-size: 2em;
-//     color: grey;
-// }
-
-// .bi-droplet {
-//     font-size: 2em;
-//     color: blue;
-// }
 
 export default function LocalizedMeteo() {
     const apiKey = '7d4a696f4e46055b073d599ec89e157b';
     // localisation from API Geolocalisation
     let location = navigator.geolocation.getCurrentPosition(
         function (position) {
-            console.log(position)
+            //console.log(position)
 
             // Call to openWeather API with city for parameter
             let apiCall = function (lat, lon) {
@@ -63,32 +66,32 @@ export default function LocalizedMeteo() {
                 fetch(url)
                     .then(response => response.json()
                         .then((data) => {   
-                            console.log(data);
+                            //console.log(data);
 
                         switch (data.weather[0].main) {
                             case 'Thunderstorm':
                                 document.querySelector('#city').innerHTML =
-                                "<i class='bi bi-geo-alt'></i> " + data.name + ", le temps est <i class='bi bi-cloud-lightning'></i>";
+                                "<i class='bi bi-geo-alt'></i> " + data.name + ", tendance <i class='bi bi-cloud-lightning'></i>";
                                 break;
                             case 'Clouds':
                                 document.querySelector('#city').innerHTML =
-                                "<i class='bi bi-geo-alt'></i> " + data.name + ", le temps est <i class='bi bi-cloud'></i>";
+                                "<i class='bi bi-geo-alt'></i> " + data.name + ", tendance <i class='bi bi-cloud'></i>";
                                 break;
                             case 'Rain':
                                 document.querySelector('#city').innerHTML =
-                                "<i class='bi bi-geo-alt'></i> " + data.name + ", le temps est <i class='bi bi-cloud-drizzle'></i>";
+                                "<i class='bi bi-geo-alt'></i> " + data.name + ", tendance <i class='bi bi-cloud-drizzle'></i>";
                                 break;
                             case 'Clear':
                                 document.querySelector('#city').innerHTML =
-                                "<i class='bi bi-geo-alt'></i> " + data.name + ", le temps est <i class='bi bi-brightness-high'></i>";
+                                "<i class='bi bi-geo-alt'></i> " + data.name + ", tendance <i class='bi bi-brightness-high'></i>";
                                 break;
                             case 'Mist' || 'Smoke' || 'Haze' || 'Dust' || 'Fog' || 'Sand' || 'Ash' || 'Squall' || 'Tornado':
                                 document.querySelector('#city').innerHTML =
-                                "<i class='bi bi-geo-alt'></i> " + data.name + ", le temps est <i class='bi bi-cloud-drizzle'></i>";
+                                "<i class='bi bi-geo-alt'></i> " + data.name + ", tendance <i class='bi bi-cloud-drizzle'></i>";
                                 break;
                             case 'Snow':
                                 document.querySelector('#city').innerHTML =
-                                "<i class='bi bi-geo-alt'></i> " + data.name + ", le temps est <i class='bi bi-cloud-snow'></i>";
+                                "<i class='bi bi-geo-alt'></i> " + data.name + ", tendance <i class='bi bi-cloud-snow'></i>";
                                 break;              
                             default:
                                 document.querySelector('#city').innerHTML = "Tendance : inconnue";
@@ -96,7 +99,7 @@ export default function LocalizedMeteo() {
                         }
 
                         document.querySelector('#temp').innerHTML =
-                            "<i class='bi bi-thermometer-half'></i> " + data.main.temp + '°C' + ", ressenti " + data.main.feels_like + '°C';
+                            "<i class='bi bi-thermometer-half'></i> "+ data.main.temp + '°C' + ", ressenti " + data.main.feels_like + '°C';
                         document.querySelector('#wind').innerHTML =
                             "<i class='bi bi-wind'></i> " + data.wind.speed + ' km/h';
                         document.querySelector('#humidity').innerHTML =
@@ -114,10 +117,10 @@ export default function LocalizedMeteo() {
             <Wrapper>
                 <Title>Météo du moment</Title>
                 <WeatherWrapper>
-                    <WeatherTile id="city">Ville: </WeatherTile>
-                    <WeatherTile id="temp">Température: </WeatherTile>
-                    <WeatherTile id="wind">Vent: </WeatherTile>
-                    <WeatherTile id="humidity">Taux d'humidité: </WeatherTile>
+                    <WeatherTile id="city" backgroundurl="src/assets/img/map.webp">Ville: </WeatherTile>
+                    <WeatherTile id="temp" backgroundurl="src/assets/img/weather.webp">Température: </WeatherTile>
+                    <WeatherTile id="wind" backgroundurl="src/assets/img/wind.webp">Vent: </WeatherTile>
+                    <WeatherTile id="humidity" backgroundurl="src/assets/img/drops.jpg">Taux d'humidité: </WeatherTile>
                 </WeatherWrapper>
             </Wrapper>
       
--- src/components/SearchWeather.jsx
+++ src/components/SearchWeather.jsx
@@ -1,9 +1,25 @@
 import styled from 'styled-components';
+import { FaSearch } from "react-icons/fa";
+import { BsFillGeoAltFill } from "react-icons/bs";
 
 const Wrapper = styled.div`
     border-bottom: 5px solid white;
     padding: 3rem;
     margin: 0rem 4rem;
+    display: flex;
+    justify-content: space-around;
+
+    @media (max-width: 900px) {
+        flex-wrap: wrap;
+        justify-content: space-around;
+        gap: 2rem;
+    }
+`;
+
+const WrapperSearch = styled.div`
+    display: flex;
+    justify-content: center;
+    gap: 2rem;
 `;
 
 const InputSeach = styled.input`
@@ -12,16 +28,33 @@ const InputSeach = styled.input`
     padding: 10px;
 `;
 
+const LocationButton = styled.button`
+    border-radius: 0.5rem;
+    height: 3em;
+    width: 3em;
+    background-color: #5f5d5d;
+`;
+
 const BtnSearch = styled.button`
     border-radius: 0.5rem;
+    height: 3em;
+    width: 3em;
+    background-color: #5f5d5d;
 `;
 
 
 export default function SearchWeather() {
   return (
       <Wrapper>
-          <InputSeach type="text" id="inputCity" placeholder="Quel temps fait-il  à ..."></InputSeach>
-          <BtnSearch><i class="bi bi-search"></i></BtnSearch>
+          <LocationButton title="Me géolocaliser">
+              <BsFillGeoAltFill style={{ color: 'blue', fontSize: '24px' }} />
+          </LocationButton>
+          <WrapperSearch>
+            <InputSeach type="text" id="inputCity" title="Rechercher" placeholder="Quel temps fait-il  à ..."></InputSeach>
+            <BtnSearch>
+                <FaSearch style={{ color: 'blue', fontSize: '24px' }} />
+            </BtnSearch>    
+          </WrapperSearch>
       </Wrapper>
   )
 }
